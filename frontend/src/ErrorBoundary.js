66958718a35a3ca83c529c6a5dd10ec6e7a8b438
allachaib54 <allachaib54@gmail.com>
2024-07-31 08:18:52 +0100

    fix error in iphone2

--- frontend/src/ErrorBoundary.js
+++ frontend/src/ErrorBoundary.js
@@ -1,14 +1,13 @@
-// ErrorBoundary.js
 import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
     constructor(props) {
         super(props);
-        this.state = { hasError: false };
+        this.state = { hasError: false, errorMessage: '' };
     }
 
     static getDerivedStateFromError(error) {
-        return { hasError: true };
+        return { hasError: true, errorMessage: error.message };
     }
 
     componentDidCatch(error, errorInfo) {
@@ -17,10 +16,11 @@ class ErrorBoundary extends Component {
 
     render() {
         if (this.state.hasError) {
-            return <h1>Something went wrong.</h1>;
+            return <h1>{this.state.errorMessage}</h1>;
         }
         return this.props.children;
     }
 }
 
 export default ErrorBoundary;
+
